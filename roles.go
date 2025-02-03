12b341e05cbac4bbddfc042a7b74dae3fcb253f8
Mehdi Kharraz <kharraz.mehdi@gmail.com>
2025-02-03 23:39:45 +0100

    Avoid nullable Role.Aliases by initializing to an empty slice

--- roles.go
+++ roles.go
@@ -56,6 +56,7 @@ func (s *RolesService) GetRole(id int) (*Role, error) {
 	role := new(Role)
 
 	role.ID = id
+	role.Aliases = []*RoleAlias{}
 	role.URL, _ = url.JoinPath(baseURL, urlPath)
 
 	// Check for error page
